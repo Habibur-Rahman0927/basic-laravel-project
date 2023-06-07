f215f6f1f2fa4652c1091c16262837005fca11ff
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-06-07 22:59:58 +0600

    upload profile image and showing it using jquery

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <div class="page-content">
         <div class="container-fluid">
 
@@ -9,35 +10,35 @@
                         <div class="card-body">
 
                             <h4 class="card-title">Edit Profile page</h4>
-                            <form action="" method="POST">
+                            <form action="{{ route('update.profile') }}" method="POST">
+                                @csrf
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label">Name</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" id="name" value="{{ $editData->name }}"
-                                            name="name" type="text" placeholder="Enter your name..."
-                                            id="example-text-input">
+                                            name="name" type="text" placeholder="Enter your name...">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label">Email</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" id="email" value="{{ $editData->email }}"
-                                            name="email" type="email" placeholder="Enter your email..."
-                                            id="example-text-input">
+                                            name="email" type="email" placeholder="Enter your email...">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" id="profile_image" name="profile_image" type="file"
-                                            placeholder="Enter Profile Image..." id="example-text-input">
+                                            placeholder="Enter Profile Image...">
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                                     <div class="col-sm-10">
-                                        <img class="rounded avatar-lg mt-1" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                                        <img id="showImage" class="rounded avatar-lg mt-1"
+                                            src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
                                     </div>
                                 </div>
                                 <input type="submit" class="btn btn-info waves-effect waves-light" value="Update Profile">
@@ -52,4 +53,16 @@
 
         </div> <!-- container-fluid -->
     </div>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#profile_image').change(function(e) {
+                e.preventDefault();
+                var reader = new FileReader()
+                reader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files[0])
+            });
+        });
+    </script>
 @endsection
